6fa448224489b501779bdf7f1eb9a09a9ada4126
Manel R. Doménech <manel@ecomm.pro>
2019-12-23 16:24:08 +0100

    Add PHP 7.4

--- src/versions.php
+++ src/versions.php
@@ -30,12 +30,10 @@ return [
     [
         'version' => '7.3-fpm',
     ],
-    /*
     [
-        'version' => '7.4-rc-cli',
+        'version' => '7.4-cli',
     ],
     [
-        'version' => '7.4-rc-fpm',
+        'version' => '7.4-fpm',
     ],
-    */
 ];
