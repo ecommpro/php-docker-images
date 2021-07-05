704825a70842a5447e2aaa27a1ca3cd5d72769a4
Manel R. Doménech <manel@ecomm.pro>
2021-07-05 22:55:19 +0200

    Push all images after build

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 TO=$(pwd)/cli-tools ./update-cli-tools.sh
 docker run -u$(id -u):$(id -g) -ti --rm -v $(pwd):/work php:7.3-cli-alpine -f /work/build.php
 ls build | xargs -P4 -I{} sh -c "docker build -t ecommpro/php:{} -f build/{}/Dockerfile ."
-docker push ecommpro/php
+docker push -a ecommpro/php
